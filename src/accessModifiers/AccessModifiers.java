e6b966282fba7bed978180338d7b5a979f86202a
Nahidur Rahman Tushar <tusharnahid562@gmail.com>
2025-04-28 15:11:38 +0600

    for access modifier

---
+++ src/accessModifiers/AccessModifiers.java
@@ -0,0 +1,11 @@
+package accessModifiers;
+
+public class AccessModifiers {
+    private String name;
+    void setName(String countryName) {
+       name = countryName;
+    }
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/accessModifiers/tushar02.java
@@ -0,0 +1,9 @@
+package accessModifiers;
+
+public class tushar02 {
+    public static void main(String[] args) {
+        AccessModifiers obj = new AccessModifiers();
+        obj.setName("Switzerland , Palestine ");
+        System.out.print(obj.getName());
+    }
+}
