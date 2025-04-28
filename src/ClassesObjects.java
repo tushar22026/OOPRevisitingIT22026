1d1622edbbe10a022717e4eaa758a19096231b43
Nahidur Rahman Tushar <tusharnahid562@gmail.com>
2025-04-28 14:20:17 +0600

    output

---
+++ src/ClassesObjects.java
@@ -0,0 +1,8 @@
+public class ClassesObjects {
+    String name = "Nahidur Rahman Tushar";
+    int age = 22;
+    public void printNameAndAge() {
+        System.out.println("Name: " + name + "\n" + "Age: " + age);
+    }
+}
+
